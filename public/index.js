2c968a30b3dde066f90b450e052e058772177994
Cu Ly <61442956+culy247@users.noreply.github.com>
2020-05-05 16:35:58 +0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -11,17 +11,59 @@ var sessionDescription = window.RTCSessionDescription ||
     window.webkitRTCSessionDescription ||
     window.msRTCSessionDescription;
 
-    navigator.getUserMedia  = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
+if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
+}
+
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia = navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia;
+
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function(resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+    }
+}
+
+function streamHandler(stream) {
+    var video = document.querySelector('video');
+    video.srcObject = stream;
+    pc.addStream(stream);
+}
+
+function errorHandler(error){
+    console.log( error );
+}
+
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true
+})
+.then(streamHandler)
+.catch(errorHandler);
+
+
 
 var pc = new peerConnection({
-    iceServers: [{
-        url: "stun:stun.services.mozilla.com",
-        username: "somename",
-        credential: "somecredentials"
-    }]
+    iceServers: [
+        { url: "stun:stun.l.google.com:19302" },
+        { url: "stun:stun.stunprotocol.org" }
+    ]
 });
 
 pc.onaddstream = function (obj) {
@@ -32,12 +74,6 @@ pc.onaddstream = function (obj) {
     document.getElementById('users-container').appendChild(vid);
     vid.srcObject = obj.stream;
 }
-
-navigator.getUserMedia({video: true, audio: true}, function (stream) {
-    var video = document.querySelector('video');
-    video.srcObject = stream;
-    pc.addStream(stream);
-}, error);
 
 
 socket.on('add-users', function (data) {
